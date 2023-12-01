b7b01180013cac8f3a5853bbeb32cc614d0b65a6
Ravi Bhoraskar <>
2023-11-30 23:05:32 -0800

    Day 1.1

---
+++ day1.py
@@ -0,0 +1,15 @@
+import fileinput
+
+def first_digit(s):
+    for c in s:
+        if c.isdigit():
+            return c
+    return -1
+
+total = 0
+for line in fileinput.input():
+    first = first_digit(line)
+    last = first_digit(line[::-1])
+    num  = int(first + last)
+    total += num
+print(total)
