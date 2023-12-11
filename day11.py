13019a7ecb14084b6f3df31996e50cf4b7f6b56c
Ravi Bhoraskar <>
2023-12-10 21:23:36 -0800

    day11 part 1

---
+++ day11.py
@@ -0,0 +1,37 @@
+import fileinput
+import numpy
+
+def print_matrix(m):
+    for row in m:
+        print(''.join(row))
+
+input = fileinput.input()
+output = 0
+m = []
+
+for line in input:
+    line = line.strip()
+    if not '#' in line:
+        m.append(list(line))
+    m.append(list(line))
+
+m = numpy.transpose(m)
+newm = []
+for line in m:
+    if not '#' in line:
+        newm.append(line)
+    newm.append(line)
+m = numpy.transpose(newm)
+
+galaxies = []
+for i, _ in enumerate(m):
+    for j, e in enumerate(m[i]):
+        if (e == '#'):
+            galaxies.append((i, j))
+
+sum = 0
+for g1 in galaxies:
+    for g2 in galaxies:
+        if g1 < g2:
+            sum += abs(g1[0] - g2[0]) + abs(g1[1] - g2[1])
+print(sum)
